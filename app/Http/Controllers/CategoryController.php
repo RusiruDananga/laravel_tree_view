f5a476cc0d2925af2efd546bf32dfeac2f0c43a6
RusiruDananga <rusirudananga97@gmail.com>
2023-02-15 23:01:14 +0530

    commit 2/15/2023

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $parentCategories = Category::where('parent_id',0)->get();
+        return view('category.index', compact('parentCategories'));
+    }
+}
